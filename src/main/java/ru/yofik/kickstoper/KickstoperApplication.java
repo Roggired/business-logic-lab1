d8d4d0b8fcd7afc102b6ad072f4c10cbbafaba71
Roggired <leshaego14@gmail.com>
2022-02-20 16:45:06 +0300

    Initial commit

---
+++ src/main/java/ru/yofik/kickstoper/KickstoperApplication.java
@@ -0,0 +1,13 @@
+package ru.yofik.kickstoper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickstoperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickstoperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yofik/kickstoper/KickstoperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yofik.kickstoper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickstoperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
