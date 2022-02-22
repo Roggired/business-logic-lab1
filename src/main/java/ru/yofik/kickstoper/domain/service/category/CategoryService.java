91365571f43a10549946cdb526f7df9703cb99e3
Roggired <leshaego14@gmail.com>
2022-02-22 23:49:04 +0300

    Add endpoint for getAllCategories and getCategory. Fix bug with serialization of lazy load Hibernate proxy

--- src/main/java/ru/yofik/kickstoper/domain/service/category/CategoryService.java
+++ src/main/java/ru/yofik/kickstoper/domain/service/category/CategoryService.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Service;
 import ru.yofik.kickstoper.domain.entity.category.Category;
 
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Service
 public interface CategoryService {
     boolean isExist(int id);
     @NotNull Category getById(int id);
+    @NotNull List<Category> getAllCategories();
 }
